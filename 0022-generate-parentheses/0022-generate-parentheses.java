a133128a8fa017389426ead1fe3a426980544dcc
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-03-20 12:55:24 +0530

    Time: 0 ms (100%), Space: 44.4 MB (85.09%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        backtrack(res, new StringBuilder(), 0, 0, n);
+        return res;
+    }
+
+    private void backtrack(List<String> res, StringBuilder current, int open, int close, int n) {
+       
+        if (current.length() == 2 * n) {
+            res.add(current.toString());
+            return;
+        }
+
+       
+        if (open < n) {
+            current.append('(');
+            backtrack(res, current, open + 1, close, n);
+            current.deleteCharAt(current.length() - 1); 
+        }
+
+        if (close < open) {
+            current.append(')');
+            backtrack(res, current, open, close + 1, n);
+            current.deleteCharAt(current.length() - 1); 
+        }
+    }
+}
