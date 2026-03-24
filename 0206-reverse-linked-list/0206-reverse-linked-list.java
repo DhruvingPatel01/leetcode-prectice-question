c392240ec392ab3f16fe0ac225e90c55095559e8
DhruvingPatel01 <2403031080035@paruluniversity.ac.in>
2026-03-24 08:37:50 +0530

    Time: 0 ms (100%), Space: 44.5 MB (9.32%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -12,12 +12,12 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null;
         ListNode curr = head;
-        while (curr != null){
+        while (curr != null) {
             ListNode nextTemp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = nextTemp;
-        }    
+        }
         return prev;
     }
 }
